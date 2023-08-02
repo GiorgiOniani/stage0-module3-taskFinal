1c7c58a76dbbb49beb0b0fca8d96cdf67b0926bc
giorgi.oniani.2 <giorgi.oniani.2@btu.edu.ge>
2023-08-03 01:30:54 +0400

    80

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int num1 = (number / 100) % 10;
+        int num2 = (number / 10) % 10;
+        int num3 = number % 10;
+
+        int dalagNum = num3 * 100  + num2 * 10  + num1;
+
+        System.out.println(dalagNum);
+
+
+
+
     }
 }
