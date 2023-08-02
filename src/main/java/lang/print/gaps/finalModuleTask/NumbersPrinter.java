3e5e1c845963c1786aa247f9b51d241f8b1ff0d3
GiorgiOniani <93796850+GiorgiOniani@users.noreply.github.com>
2023-08-03 02:01:56 +0400

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,8 +3,8 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
         boolean check = (number > 0) | (number < 0);
-        String result = check? "true" : "false";
-        System.out.println(result);
+        
+        System.out.println(check);
 
     }
 }
